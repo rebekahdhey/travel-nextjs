bc45e02b2da0311cca098944f91e1426896db4d5
Rebekah Tañag <rebekah.tanag@edufied.edu.vn>
2024-10-22 00:48:25 +0800

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      // Next.js automatically optimizes images in the 'public' directory
+      // You don't need to add anything here for local images
+      unoptimized: false, // Optional: set to true if you want to disable optimization
+    },
+  };
+  
+  export default nextConfig;
+  
